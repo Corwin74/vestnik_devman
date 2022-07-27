af0ee5e16114a117efef98508246dc7adfccf09c
Alexander Samokhin <samokhin@shockland.ru>
2022-07-27 19:21:12 +0300

    refactor code

--- bot.py
+++ bot.py
@@ -4,61 +4,61 @@ from environs import Env
 from telegram import Update
 from telegram.ext import CommandHandler, Updater, CallbackContext
 
-
-env = Env()
-env.read_env()
+logger = logging.getLogger(__file__)
 
 DVMN_URL = 'https://dvmn.org/api/long_polling/'
-DVMN_TOKEN = env('DVMN_TOKEN')
-BOT_TOKEN =  env('BOT_TOKEN')
-CHAT_ID =  env('CHAT_ID')
 
-
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
-
-def send_message(context, message):
-    context.bot.send_message(chat_id=CHAT_ID, text=message)
 
 def start(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id,
     text=f"Hello, {update.effective_chat.first_name}!")
 
 def check_devman_status(context: CallbackContext):
+    dvmn_token, chat_id = context.job.context
     last_time = context.bot_data.get('last_time', '')
-    headers = {'Authorization': DVMN_TOKEN}
+    headers = {'Authorization': dvmn_token}
     params = {'timestamp': last_time}
     try:
         response = requests.get(DVMN_URL, headers=headers, params=params)
         response.raise_for_status()
-        resp = response.json()
-        if resp['status'] == 'found':
-            for attempt in resp['new_attempts']:
+        review = response.json()
+        if review['status'] == 'found':
+            for attempt in review['new_attempts']:
                 if attempt['is_negative']:
                     status = 'К сожалению в работе нашлись ошибки.'
                 else:
                     status = 'Преподавателю все понравилось. Можно приступать к следующему уроку!'
                 lesson_title = attempt['lesson_title']
                 lesson_url = attempt['lesson_url']
-                send_message(context,
-                            f'У вас проверили работу "{lesson_title}"\n\n{status}\n{lesson_url}')
-        last_time = resp.get('timestamp_to_request', last_time)
-        last_time = resp.get('last_attempt_timestamp', last_time)
+                context.bot.send_message(chat_id=chat_id, text=f'У вас проверили работу \
+                                         "{lesson_title}"\n\n{status}\n{lesson_url}')
+        last_time = review.get('timestamp_to_request', last_time)
+        last_time = review.get('last_attempt_timestamp', last_time)
         context.bot_data['last_time'] = last_time
-    except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError):
-        print('Error connecting to Devamn server!')
-    except KeyError as exc:
-        print('Key error!')
-        print(exc)
+    except requests.exceptions.ReadTimeout:
+        pass
+    except requests.exceptions.ConnectionError:
+        print('Connection Error!')
 
 
-updater = Updater(token=BOT_TOKEN, use_context=True)
-dispatcher = updater.dispatcher
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+def main():
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+    env = Env()
+    env.read_env()
+    dvmn_token = env('DVMN_API_TOKEN')
+    tlgm_bot_token =  env('TLGM_BOT_TOKEN')
+    tlgm_chat_id =  env('TLGM_CHAT_ID')
+    updater = Updater(token=tlgm_bot_token, use_context=True)
+    dispatcher = updater.dispatcher
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
 
-j = updater.job_queue
-j.run_repeating(check_devman_status, 300, 10)
+    j = updater.job_queue
+    j.run_repeating(check_devman_status, 300, 10, context=(dvmn_token, tlgm_chat_id))
 
-updater.start_polling()
+    #updater.start_polling()
+
+
+if __name__ == '__main__':
+    main()
