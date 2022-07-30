262d1359b29d5e81bfc9cea8fb06e33ac226ecd5
Sammy <sammy@shockland.ru>
2022-07-30 15:13:25 +0000

    catch errors and write to log

--- bot.py
+++ bot.py
@@ -11,15 +11,39 @@ DVMN_URL = 'https://dvmn.org/api/long_polling/'
 SLEEP_TIME = 600
 
 
+class TlgmLogsHandler(logging.Handler):
+    def __init__(self, token, chat_id):
+        super().__init__()
+        self.bot = telegram.Bot(token=token)
+        self.admin_chat_id = chat_id
+
+    def emit(self, record):
+        self.bot.send_message(
+                         chat_id=self.admin_chat_id,
+                         text=self.format(record)
+        )
+
+
 def main():
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
     env = Env()
     env.read_env()
     dvmn_token = env('DVMN_API_TOKEN')
     tlgm_bot_token = env('TLGM_BOT_TOKEN')
     tlgm_chat_id = env('TLGM_CHAT_ID')
-    bot = telegram.Bot(token=tlgm_bot_token)
+
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    logger.setLevel(logging.INFO)
+    tlgm_handler = TlgmLogsHandler(tlgm_bot_token, tlgm_chat_id)
+    logger.addHandler(tlgm_handler)
+    logger.info('Started A Bot "Vestnik Devman".')
+
+    try:
+        0/0
+    except Exception:
+        logging.exception()
+
     while True:
         last_time = ''
         headers = {'Authorization': dvmn_token}
@@ -33,18 +57,26 @@ def main():
                     if attempt['is_negative']:
                         status = 'К сожалению в работе нашлись ошибки.'
                     else:
-                        status = 'Преподавателю все понравилось.' + \
-                                 'Можно приступать к следующему уроку!'
+                        status = (
+                                  'Преподавателю все понравилось.'
+                                  'Можно приступать к следующему уроку!'
+                        )
                     lesson_title = attempt['lesson_title']
                     lesson_url = attempt['lesson_url']
-                    bot.send_message(chat_id=tlgm_chat_id,
-                        text=f'У вас проверили работу "{lesson_title}"\n\n{status}\n{lesson_url}')
+                    tlgm_handler.bot.send_message(
+                        chat_id=tlgm_chat_id,
+                        text=(
+                              f'У вас проверили работу "{lesson_title}"\n\n'
+                              f'{status}\n{lesson_url}'
+                        )
+                    )
             last_time = review.get('timestamp_to_request', last_time)
             last_time = review.get('last_attempt_timestamp', last_time)
         except requests.exceptions.ReadTimeout:
             pass
         except requests.exceptions.ConnectionError:
             time.sleep(SLEEP_TIME)
+        pass
 
 
 if __name__ == '__main__':
